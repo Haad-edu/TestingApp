0b12280d17f1b36111f74225c05cb36bcb805b10
Rustam <rustamismatov45@gmail.com>
2023-12-20 18:14:21 +0500

    UserPaswordAttribute has been created

---
+++ src/TestingApp.Service/Attributes/UserPasswordAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestingApp.Service.Attributes
+{
+    public class UserPasswordAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value,ValidationContext validationContext)
+        {
+            if (value is string password && password.Length >= 8 && password.Any(c => char.IsDigit(c))
+                && password.Any(c => char.IsLetter(c))); 
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult("Sizning parolingiz yaroqli emas");
+        }
+
+        /*protected ValidationResult IsReliable(object value)
+        {
+            if (value is string password && password.Length >= 8 && password.Any(c => char.IsDigit(c))
+                && password.Any(c => char.IsLetter(c))) ;
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult("");
+        }*/
+    }
+}
