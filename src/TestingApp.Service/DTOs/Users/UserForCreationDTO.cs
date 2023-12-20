784e6a8ef7b4aaa2a937a3642149aa8a27cb5e89
Muhammadamin Omonov <muhammadomonov2004@gmail.com>
2023-12-20 20:46:47 +0500

    UserForCreationDTO is changed

--- src/TestingApp.Service/DTOs/Users/UserForCreationDTO.cs
+++ src/TestingApp.Service/DTOs/Users/UserForCreationDTO.cs
@@ -1,9 +1,11 @@
-﻿using TestingApp.Domain.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using TestingApp.Domain.Enums;
 
 namespace TestingApp.Service.DTOs.Users;
 
 public class UserForCreationDTO
 {
+    [MaxLength(64, ErrorMessage = "First Name must be longer than 64 characters")]
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
