b3ee4efbebd1c162ca12fd8b16333ad4ac033eb2
Muhammadamin Omonov <muhammadomonov2004@gmail.com>
2023-12-20 15:57:27 +0500

    Testing of Github

---
+++ src/TestingApp.Service/DTOs/UserDTOs/UserCreationDTO.cs
@@ -0,0 +1,12 @@
+﻿using TestingApp.Domain.Enums;
+
+namespace TestingApp.Service.DTOs.UserDTOs;
+
+public class UserCreationDTO
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public UserRole UserRole { get; set; }
+}
