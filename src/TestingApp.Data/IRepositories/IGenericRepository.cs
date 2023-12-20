cea243fca48316748d2852eab6602cdd888c430c
unknown <bozorovsardor0610@gmail.com>
2023-12-19 18:49:10 -0800

    IGeneric Repositoriy has been created

---
+++ src/TestingApp.Data/IRepositories/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingApp.Data.IRepositories
+{
+    internal class IGenericRepository
+    {
+    }
+}
