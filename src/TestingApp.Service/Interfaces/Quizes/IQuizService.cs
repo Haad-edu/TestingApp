85d33b2a93806aa8a434fc5a605337409fec5c4c
Azizbek <adkhamovaz26@gmail.com>
2023-12-25 18:00:29 +0500

    IQuizServiceAdded

---
+++ src/TestingApp.Service/Interfaces/Quizes/IQuizService.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+using TestingApp.Domain.Configurations;
+using TestingApp.Domain.Entities.Quizes;
+using TestingApp.Service.DTOs.Quizes;
+
+namespace TestingApp.Service.Interfaces.Quizes;
+public interface IQuizService
+{
+    ValueTask<QuizForViewDTO> CreateAsync(QuizForCreationDTO quizForCreationDTO);
+
+    ValueTask<QuizForViewDTO> UpdateAsync(int id, QuizForCreationDTO quizForCreationDTO);
+
+    ValueTask<bool> DeleteAsync(int id);
+
+    ValueTask<IEnumerable<QuizForViewDTO>> GetAllAsync( Expression<Func<Quiz, bool>> expression = null);
+
+    ValueTask<QuizForViewDTO> GetAsync(Expression<Func<Quiz, bool>> expression);
+
+
+}
