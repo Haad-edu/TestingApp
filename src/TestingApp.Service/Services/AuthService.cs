80cb6db30e128190228d883fb47849da19a782d7
Fazliddin <Fazliddin@DESKTOP-9RQEV5Q>
2023-12-25 21:43:26 +0500

    AuthService created

---
+++ src/TestingApp.Service/Services/AuthService.cs
@@ -0,0 +1,12 @@
+﻿using TestingApp.Service.Interfaces.Users;
+
+namespace TestingApp.Service.Services
+{
+    public class AuthService : IAuthService
+    {
+        public ValueTask<string> GenerateToken(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
