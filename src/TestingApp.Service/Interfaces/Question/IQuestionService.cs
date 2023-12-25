993728bfb10812d0eec8e453bc7e82c77cf69408
Barkinoff <Jahongirbarkinov@gmail.com>
2023-12-25 16:34:42 +0500

    IQuestionService has been created, and QuestionForResultDto and QuestionForUpdateDto have been added to the DTOs folder

---
+++ src/TestingApp.Service/Interfaces/Question/IQuestionService.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.Linq.Expressions;
+using TestingApp.Domain.Configurations;
+using TestingApp.Service.DTOs.Quizes;
+
+namespace TestingApp.Service.Interfaces.Question;
+
+public interface IQuestionService
+{
+    public Task<QuestionForResultDTO> CreateAsync(QuestionForCreationDTO questionForCreation);
+
+    public Task<QuestionForResultDTO> UpdateAsync(QuestionForUpdateDTO questionForUpdate);
+
+    public Task<bool> DeleteAsync(int id);
+
+    public Task<QuestionForResultDTO> GetAsync(int id);
+
+    public Task<IEnumerable<QuestionForResultDTO>> GetAllAsync(PaginationParams @params);
+}
