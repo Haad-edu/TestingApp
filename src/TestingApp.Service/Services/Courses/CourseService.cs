a9645018964d117a0ea8c5e4aca0bfcf358815a6
Sardor <104214633+Askarov-S@users.noreply.github.com>
2024-01-09 20:22:16 +0500

    I changed the ToPagedList method, added AddCustomServices to program.cs and created all Course methods

---
+++ src/TestingApp.Service/Services/Courses/CourseService.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using TestingApp.Data.IGenericRepositories;
+using TestingApp.Domain.Configurations;
+using TestingApp.Domain.Entities.Courses;
+using TestingApp.Service.DTOs.Courses;
+using TestingApp.Service.Exceptions;
+using TestingApp.Service.Extensions;
+using TestingApp.Service.Interfaces.Courses;
+
+namespace TestingApp.Service.Services.Courses
+{
+    public class CourseService : ICourseService
+    {
+        private readonly IMapper mapper;
+        private readonly IGenericRepository<Course> courseRepository;
+
+        public CourseService(IMapper mapper, IGenericRepository<Course> courseRepository)
+        {
+            this.mapper = mapper;
+            this.courseRepository = courseRepository;
+        }
+
+        public async Task<CourseForCreationDTO> CreateAsync(CourseForCreationDTO courseForCreationDTO)
+        {
+            var exist = await courseRepository.GetAsync(e => e.Name == courseForCreationDTO.Name);
+            if (exist != null)
+                throw new TestingAppException(400, "Course is already exist");
+
+            var mappedCourse = mapper.Map<Course>(courseForCreationDTO);
+            mappedCourse.CreatedAt = DateTime.UtcNow;
+
+            var courses = await courseRepository.CreateAsync(mappedCourse);
+
+            return mapper.Map<CourseForCreationDTO>(courses);
+        }
+
+        public async Task<bool> DeleteAsync(long Id)
+        {
+            var existCourse = await courseRepository.DeleteAsync(i => i.Id == Id);
+
+            if (existCourse == null)
+                throw new TestingAppException(404, "Course not found");
+            return true;
+        }
+
+        public async ValueTask<IEnumerable<CourseForCreationDTO>> GetAllAsync(PaginationParams @params, Expression<Func<Course, bool>> expression = null)
+        {
+            var Courses = courseRepository.GetAll(expression: expression, isTracking: false);
+
+            return mapper.Map<IEnumerable<CourseForCreationDTO>>(await Courses.ToPagedList(@params).ToListAsync());
+        }
+
+        public async ValueTask<CourseForCreationDTO> GetAsync(Expression<Func<Course, bool>> expression = null)
+        {
+            var GetCourse = await courseRepository.GetAsync(expression);
+
+            if (GetCourse is null)
+                throw new TestingAppException(404, "Course not found ");
+
+
+            return mapper.Map<CourseForCreationDTO>(GetCourse);
+        }
+
+        public async Task<CourseForCreationDTO> UpdateAsync(long Id, CourseForCreationDTO Updatecourse)
+        {
+            var ExistCourse = await courseRepository.GetAsync(u => u.Id == Id);
+
+            if (ExistCourse is null)
+                throw new TestingAppException(404, "Course not found");
+
+            ExistCourse.UpdatedAt = DateTime.UtcNow;
+            ExistCourse = await courseRepository.Update(mapper.Map(Updatecourse, ExistCourse));
+            return mapper.Map<CourseForCreationDTO>(ExistCourse);
+        }
+    }
+}
--- src/TestingApp/Program.cs
+++ src/TestingApp/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCustomServices();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
