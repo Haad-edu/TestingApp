9e5f3a6c5bdccc1600c31d5703a3a5fef7e317ae
Sardor Bozorov <bozorovsardor0610@gmail.com>
2024-01-09 21:47:53 -0800

    QuizResult Service and Controller have been created

---
+++ src/TestingApp.Service/DTOs/Quizes/SolvedQuestionForView.cs
@@ -0,0 +1,11 @@
+﻿using TestingApp.Domain.Entities.Quizes;
+
+namespace TestingApp.Service.DTOs.Quizes;
+
+public class SolvedQuestionForView
+{
+    public Question Question { get; set; }
+    public Answer Answer { get; set; }
+    public bool IsCorrect { get; set; }
+    public int QuizResultId { get; set; }
+}
---
+++ src/TestingApp/Controllers/QuizControllers/QuizResultController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TestingApp.Domain.Configurations;
+using TestingApp.Service.DTOs.Quizes;
+using TestingApp.Service.Interfaces.Quizes;
+
+namespace TestingApp.Controllers.QuizControllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class QuizResultController : ControllerBase
+{
+    private readonly IQuizResultService quizResultService;
+    public QuizResultController(IQuizResultService quizResultService)
+    {
+        this.quizResultService = quizResultService;
+    }
+    /// <summary>
+    /// Everyone can crearte
+    /// </summary>
+    /// <param name="courseForCreationDTO"></param>
+    /// <returns></returns>
+
+    [HttpPost, Authorize(Roles = "Teacher")]
+    public async ValueTask<IActionResult> CreateAsync(QuizResultForCreationDTO courseForCreationDTO)
+        => Ok(await quizResultService.CreateAsync(courseForCreationDTO));
+
+    /// <summary>
+    /// Get a quiz result {Everyone}
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpGet("{id}")]
+    public async ValueTask<IActionResult> GetAsync([FromRoute] int id)
+        => Ok(await quizResultService.GetAsync(u => u.Id == id));
+
+    /// <summary>
+    /// Eveyone can see
+    /// </summary>
+    /// <param name="params"></param>
+    /// <returns></returns>
+    [HttpGet]
+    public async ValueTask<IActionResult> GetAll([FromQuery] PaginationParams @params)
+       => Ok(await quizResultService.GetAllAsync(@params));
+}
