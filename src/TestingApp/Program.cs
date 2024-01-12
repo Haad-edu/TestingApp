c07c843d5825c31e9b1269e76de9a71bb67c9597
Muhammadamin Omonov <muhammadomonov2004@gmail.com>
2024-01-12 17:05:31 +0500

    Add Middleware and Serilog

--- src/TestingApp/Program.cs
+++ src/TestingApp/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using TestingApp.Data.DbContexts;
 using TestingApp.Service.Mappers;
 using TestingApp.ServiceExtensions;
+using TestingSystem.Api.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Serilog
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+
+
 builder.Services.AddCustomServices();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddAutoMapper(typeof(MappingProfile));
@@ -30,6 +42,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.MapControllers();
 
